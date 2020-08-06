f1efa2449493a62d8b7786b58806384887cf8dac
Christian Valdez <christianvaldez36@hotmail.com>
2020-08-06 16:07:42 -0700

    broke out navbar to own component

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/todos">Todos</router-link>
-    </div>
+    <Navbar />
     <router-view />
   </div>
 </template>
 
+<script>
+import Navbar from './components/Navbar.vue';
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
+
 <style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: content-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-
-.text-field {
-  &__input {
-  }
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="nav">
+      <router-link class="nav__link" to="/">Home</router-link> |
+      <router-link class="nav__link" to="/todos">Todos</router-link>
+    </div>
+</template>
+
+<style lang="scss">
+.nav {
+  padding: 30px;
+
+  &__link {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
