c096741a87b04a07b75098aabf32a31118bedbef
Christian Valdez <christianvaldez36@hotmail.com>
2020-08-06 16:07:58 -0700

    can add todos locally....

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,4 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
   </div>
 </template>
--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -1,9 +1,40 @@
 <template>
   <div>
     <h1>Todos</h1>
-    <div>
-      <button></button>
-      <input type="text" placeholder="New Todo" />
+    <div class="text-field">
+      <input v-model="inputText" class="text-field__input" type="text" placeholder="New Todo" />
+      <button @click="addTodo" class="text-field__action">+</button>
+    </div>
+    <div class="todos">
+      <p v-for="(todo, index) in todos" :key="index">{{ todo.text }}</p>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputText: '',
+      todos: [{ text: '' }],
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        text: this.inputText,
+      });
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.text-field {
+  &__input {
+  }
+
+  &__action {
+  }
+}
+</style>
