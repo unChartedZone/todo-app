0088610f5679c2de00faaed8917cb5cc0b942e08
Christian Valdez <christianvaldez36@hotmail.com>
2020-08-10 22:53:06 -0700

    added more stylings for todo page

--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -2,14 +2,14 @@
   <div class="container mx-auto">
     <h1 class="h1">Todos</h1>
     <div class="text-field">
-      <form v-on:submit.prevent class="flex items-center h-full">
+      <form v-on:submit.prevent class="flex justify-between items-center h-full">
         <input v-model="inputText" class="text-field__input" type="text" placeholder="New Todo" />
         <button @click="addTodo" class="text-field__action">+</button>
       </form>
     </div>
-    <div class="todos flex flex-col">
-      <div class="todos__item" v-for="(todo, index) in todos" :key="index">
-        <p class="text-2xl">
+    <div class="todos">
+      <div class="todos__item shadow-md" v-for="(todo, index) in todos" :key="index">
+        <p class="text-2xl inline-block">
           {{ todo.text }}
         </p>
       </div>
@@ -22,7 +22,7 @@ export default {
   data() {
     return {
       inputText: '',
-      todos: [{ text: 'First Todo' }, { text: 'Second Todo' }, { text: 'Third Todo' }],
+      todos: [{ text: 'Buy milk' }, { text: 'Get food for boots' }, { text: 'Take out the trash' }],
     };
   },
   methods: {
@@ -40,11 +40,11 @@ export default {
 .text-field {
   border: 1px solid #e4d1d1;
   border-radius: 7px;
-  display: inline-block;
   font-size: 1.5rem;
 
   &__input {
-    padding: 0 0.5rem;
+    flex-grow: 1;
+    padding: 0.5rem 1rem;
     border: none;
     outline: none;
   }
@@ -57,12 +57,16 @@ export default {
 }
 
 .todos {
+  display: flex;
+  flex-direction: column;
   margin: 5rem 0;
 
   &__item {
-    border: 1px solid #333;
+    background-color: #fefefe;
+    border-radius: 7px;
     display: inline-block;
-    padding: 1rem;
+    padding: 1rem 1rem;
+    margin: 0 0 2rem;
   }
 }
 </style>
