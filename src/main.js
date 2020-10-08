f4cf674f91f69e2a6a23a3397214768969d1901f
Christian Valdez <christianvaldez36@hotmail.com>
2020-10-07 18:18:13 -0700

    added and setup vee validate

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import Vue from 'vue';
+import {
+  ValidationObserver,
+  ValidationProvider,
+  extend,
+  localize,
+} from 'vee-validate';
+import en from 'vee-validate/dist/locale/en.json';
+import { required, email, alpha } from 'vee-validate/dist/rules';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -9,6 +17,15 @@ import ZTextfield from './components/ZTextfield.vue';
 import ZButton from './components/ZButton.vue';
 
 Vue.config.productionTip = false;
+
+extend('email', email);
+extend('required', required);
+extend('alpha', alpha);
+
+localize('en', en);
+
+Vue.component('ValidationProvider', ValidationProvider);
+Vue.component('ValidationObserver', ValidationObserver);
 
 Vue.component('z-checkbox', ZCheckbox);
 Vue.component('z-textfield', ZTextfield);
